7e6b00d75b9ec4e7d343b989e1da34c8fa292abb
Yoni Mood <yonispark@gmail.com>
2018-04-06 02:53:44 +0300

    translator, lazy links

--- texts/views.py
+++ texts/views.py
@@ -1,11 +1,8 @@
-import uuid
+import json
 
-from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseNotFound
 
-# Create your views here.
-from .models import LenguaText
-import requests as r
+from translator import translate as lenuga_translate
 
 
 def translate(request):
@@ -23,34 +20,7 @@ def translate(request):
     if target is None or target == "":
         return HttpResponseNotFound('<h1>target language is not specified.</h1>')
 
-
-    text = LenguaText.objects.filter(values__icontains="¾{}½".format(original))
-
-    if len(text) == 0:
-        gt_result = get_google_reeult(key, original, target)
-        try:
-            from_language = gt_result['detectedSourceLanguage']
-            g_text = gt_result['translatedText']
-            text = LenguaText()
-            text.uuid = uuid.uuid4()
-            text.add_translation(original, from_language)
-            text.add_translation(g_text, target)
-            text.save()
-        except Exception:
-            return gt_result
-    else:
-        text = text[0]
-
-    translation_value = text.get_text(target)
-
-    if translation_value is None:
-        gt_result = get_google_reeult(key, original, target)
-        try:
-            g_text = gt_result['translatedText']
-            text.add_translation(g_text, target)
-            text.save()
-        except Exception:
-            return gt_result
+    translation_value = lenuga_translate(key=key, original=original, target=target)
 
     return JsonResponse({
         "data": {
@@ -61,20 +31,4 @@ def translate(request):
                 }
             ]
         }
-    })
-
-
-def get_google_reeult(key, q, target):
-    gt_result = r.get(r'https://www.googleapis.com/language/translate/v2', {
-        'key': 'AIzaSyDSiZkiZX4_HLXlGwrVTQv1WmUgqUbZbFc',
-        'q': q,
-        'target': target,
-    })
-    if not gt_result.ok:
-        return HttpResponseNotFound("<h1>Google Translate Error</h1><br><br>{}".format(gt_result.content))
-    try:
-        gt_result = gt_result.json()
-        gt_result = gt_result['data']['translations'][0]
-        return gt_result
-    except Exception:
-        return HttpResponseNotFound("<h1>Google Translate API mismatch</h1><br><br>{}".format(gt_result.content))
+    }, json_dumps_params={'ensure_ascii': False}, safe=False)
