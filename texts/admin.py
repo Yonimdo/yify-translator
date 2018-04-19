91d43d872312ac1fb882a8af00eab08a6d6a96d2
Yoni Mood <yonispark@gmail.com>
2018-04-19 21:11:23 +0300

    index in smart

--- texts/admin.py
+++ texts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from texts.models import LenguaText, OriginalText, Suggestion
+from texts.models import LenguaText, OriginalText, Suggestion, SmartText
 
 admin.site.register(LenguaText)
+admin.site.register(SmartText)
 admin.site.register(OriginalText)
 admin.site.register(Suggestion)
