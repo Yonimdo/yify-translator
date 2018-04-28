b4e2d56ea1bd3309a93e766a0a91fbecfa73b7f8
reuvensg-PC\reuvensg <reuvensg@gmail.com>
2018-04-28 14:22:01 +0300

    Admin changes

--- texts/admin.py
+++ texts/admin.py
@@ -2,7 +2,34 @@ from django.contrib import admin
 
 from texts.models import LenguaText, OriginalText, Suggestion, SmartText
 
-admin.site.register(LenguaText)
-admin.site.register(SmartText)
-admin.site.register(OriginalText)
-admin.site.register(Suggestion)
+
+@admin.register(LenguaText)
+class LogManagement(admin.ModelAdmin):
+    list_display = ('uuid', 'values')
+    list_filter = ('uuid', 'values')
+    search_fields = ('uuid', 'values')
+
+
+@admin.register(SmartText)
+class AuditManagement(admin.ModelAdmin):
+    readonly_fields = ('language', 'count')
+    list_display = ('count', 'text', 'language')
+    list_filter = ('count', 'text', 'language')
+    search_fields = ('count', 'text', 'language')
+
+
+@admin.register(OriginalText)
+class AuditManagement(admin.ModelAdmin):
+    readonly_fields = ('original', 'count')
+    list_display = ('count', 'original')
+    list_filter = ('count', 'original')
+    search_fields = ('count', 'original')
+
+
+@admin.register(Suggestion)
+class AuditManagement(admin.ModelAdmin):
+    readonly_fields = ('original','translation', 'count')
+    list_display = ('original', 'translation', 'user_translation', 'from_language', 'to_language', 'count')
+    list_filter = ('original', 'translation', 'user_translation', 'from_language', 'to_language', 'count')
+    search_fields = ('original', 'translation', 'user_translation')
+
