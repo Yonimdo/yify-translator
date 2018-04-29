e1f3a4765191adc94f7ec05deec99d9b05073cbd
Yoni Mood <yonispark@gmail.com>
2018-04-29 08:39:13 +0300

    fix long load on smart and original

--- texts/admin.py
+++ texts/admin.py
@@ -11,22 +11,21 @@ class LenguaTextManagement(admin.ModelAdmin):
 
 @admin.register(SmartText)
 class SmartTextManagement(admin.ModelAdmin):
-    readonly_fields = ('language', 'count')
+    readonly_fields = ('language', 'text_origin', 'count')
     list_display = ('count', 'text', 'language')
     search_fields = ('count', 'text', 'language')
 
 
 @admin.register(OriginalText)
 class OriginalTextManagement(admin.ModelAdmin):
-    readonly_fields = ('original', 'count')
+    readonly_fields = ('original', 'text', 'count')
     list_display = ('count', 'original')
     search_fields = ('count', 'original')
 
 
 @admin.register(Suggestion)
 class SuggestionManagement(admin.ModelAdmin):
-    readonly_fields = ('original','translation', 'count')
+    readonly_fields = ('original', 'translation', 'count')
     list_display = ('original', 'translation', 'user_translation', 'from_language', 'to_language', 'count')
     list_filter = ('original', 'translation', 'user_translation', 'from_language', 'to_language', 'count')
     search_fields = ('original', 'translation', 'user_translation')
-
