6f778af13908a71e4b04f7f2622cc0fbbf7179d1
Yoni Mood <yonispark@gmail.com>
2018-05-01 00:09:42 +0300

    added */%₪

--- translator.py
+++ translator.py
@@ -11,7 +11,7 @@ from texts.models import LenguaText, OriginalText, SmartText
 
 q_template = '&q={}'
 WEB_URL_REGEX = r'(([ ,\.。។।။]+)?(http|ftp|https?\:?\/?\/?)?([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?([ ,\.。។।။]+)?|(([ ,\.。។।။]+)?(content\:\/\/)([\w.,@?^=%&:/~+#-]+)([ ,\.。។।။]+)?))'
-NUMBERS_REGEX = r'(([0-9A-Z\-\+:=_ ]+)?[0-9]+([A-Z\-\+:=_ 0-9]+)?)'
+NUMBERS_REGEX = r'(([0-9A-Z\-\+/:=_ */%₪]+)?[0-9]+([A-Z\-\+:=_ */%₪0-9]+)?)'
 DOTS_REGEX = re.compile(r'(\<br\>|[\.。។।။]+ ?)')
 MARKS_REGEX = re.compile(r'( ?[?？؟՞፧!]+ ?)')
 TRANSLATABLE = 'text'
